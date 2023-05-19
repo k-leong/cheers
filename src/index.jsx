65948d692b5ec3b66bca849e36878c36d6fbb6b4
kleong0221 <kenny.leong0221@yahoo.com>
2023-05-18 18:49:57 -0700

    index.jsx edit

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.DEV ? '/' : '/cheers/'}>
     <App />
   </BrowserRouter>
 )

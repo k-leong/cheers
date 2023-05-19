672541f534d822eb22bfbdf63277b5d3c3da1ad3
kleong0221 <kenny.leong0221@yahoo.com>
2023-05-18 19:20:46 -0700

    deployment edits

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BrowserRouter basename={import.meta.env.DEV ? '/' : '/cheers/'}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
 
